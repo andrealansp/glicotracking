82948f2b495c6209686a8a1ac558a58cae7d5033
a.alves <andrealansp@hotmail.com>
2025-12-11 15:08:10 -0300

    Colocando para carregas os secrets para uso no build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ set -e
 # Lê secrets em runtime e seta envs (não vaza na imagem)
 export SECRET_KEY=$(cat /run/secrets/secret_key)
 export DATABASE_URL=$(cat /run/secrets/database_url)
+export DB_USER=$(cat /run/secrets/db_user)
+export DB_NAME=$(cat /run/secrets/db_name)
+export DB_PASSWORD=$(cat /run/secrets/db_password)
 
 # Aplica migrações Django (aguarda DB pronto)
 echo "Aguardando banco de dados..."
-until PGPASSWORD=$DB_PASSWORD_GTK psql -h "bancodados" -U "${DB_USER:-postgres}" -d "${DB_NAME_GTK:-postgres}" -c '\q'; do
+until PGPASSWORD=DB_PASSWORD psql -h "bancodados" -U "${DB_USER:-postgres}" -d "${DB_NAME:-postgres}" -c '\q'; do
   >&2 echo "Banco de dados não está pronto - aguardando..."
   sleep 1
 done
