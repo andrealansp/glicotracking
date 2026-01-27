82c1a1da7fc61c18961dd67e8c57ab79a0465a30
a.alves <andrealansp@hotmail.com>
2026-01-27 16:23:11 -0300

    Corrigindo erro com o exames

--- app/settings.py
+++ app/settings.py
@@ -12,13 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+
 import dj_database_url
+from dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -74,6 +76,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_cleanup.apps.CleanupConfig",
     "core",
     "contas",
     "django_bootstrap5",
@@ -127,7 +130,7 @@ DATABASES = {
     'default': {}
 }
 
-DATABASE_URL= get_secret('database_url')
+DATABASE_URL = get_secret('database_url')
 
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
@@ -176,13 +179,12 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.hostinger.com'
-EMAIL_PORT = 465 # Use 465 for SSL or 587 for TLS
-EMAIL_USE_SSL = True # Set to True for port 465, False for port 587
-EMAIL_USE_TLS = False # Set to True for port 587, False for port 465
-EMAIL_HOST_USER = os.getenv("EMAIL") # Replace with your email
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_SENHA") # Replace with your email password
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL") # The default sender email
-
+EMAIL_PORT = 465  # Use 465 for SSL or 587 for TLS
+EMAIL_USE_SSL = True  # Set to True for port 465, False for port 587
+EMAIL_USE_TLS = False  # Set to True for port 587, False for port 465
+EMAIL_HOST_USER = os.getenv("EMAIL")  # Replace with your email
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_SENHA")  # Replace with your email password
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL")  # The default sender email
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
